712665f93052cd380e05acca1acdabcbf9c41908
Mbaye THIAM <mbathiam@bouyguestelecom.fr>
2024-05-02 14:42:48 +0200

    🐛 fix build docker image script

--- packages/graphql-mesh/scripts/build-local-image.sh
+++ packages/graphql-mesh/scripts/build-local-image.sh
@@ -7,5 +7,5 @@ cp -r ../../patches/* ./patches
 # build the docker image
 docker build -t graphql-mesh .
 # Remove the patches from the docker context
-rm -rf patches/@graphql-tools+batch-execute+9.0.2.patch patches/@graphql-tools+executor+1.2.0.patch patches/graphql+16.8.1.patch
+rm -rf patches/@graphql-tools+batch-execute+*.patch patches/@graphql-tools+executor+*.patch patches/graphql+*.patch
 
