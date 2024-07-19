0f762c3f2d58cc0f8f227c21f7bc773ed5005ccc
Adrien FIGARD <adfigard@bouyguestelecom.fr>
2024-07-19 09:52:08 +0200

    :bug: Fix filter-null plugin

--- packages/graphql-mesh/custom-plugins/filter-null.ts
+++ packages/graphql-mesh/custom-plugins/filter-null.ts
@@ -9,10 +9,17 @@ export default ({ filter }): Plugin => {
       return {
         onExecuteDone({ result, setResult }) {
           if (filter && (result as ObjMap<{ data: Object }>).data !== null) {
-            setResult({
-              ...result,
-              data: filterNullValues((result as ObjMap<{ data: Object }>).data)
-            })
+            if (isDataEmpty((result as ObjMap<{ data: Object }>).data)) {
+              delete (result as ObjMap<{ data: Object }>).data
+              setResult({
+                ...result
+              })
+            } else {
+              setResult({
+                ...result,
+                data: filterNullValues((result as ObjMap<{ data: Object }>).data)
+              })
+            }
           }
         }
       }
@@ -29,4 +36,24 @@ const filterNullValues = (obj: { data: Object | string[] }) => {
     }
   })
   return obj
+}
+
+const isDataEmpty = (obj: { data: Object | string[] }) => {
+  const checkProperties = (value: any): boolean => {
+    if (value === null || value === undefined) {
+      return true
+    }
+    if (typeof value !== 'object') {
+      return false
+    }
+    for (let key in value) {
+      if (value.hasOwnProperty(key)) {
+        if (!checkProperties(value[key])) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+  return checkProperties(obj)
 }
