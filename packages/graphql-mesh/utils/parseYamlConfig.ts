6af9ad615cdcafa1a794f2ab3c9cb93f9e413024
Thierry DEGREMONT <thdegrem@bouyguestelecom.fr>
2024-08-02 16:28:04 +0200

    add sources config file to have a separate config file for sources configuration

--- packages/graphql-mesh/utils/parseYamlConfig.ts
+++ packages/graphql-mesh/utils/parseYamlConfig.ts
@@ -1,33 +1,54 @@
 import { YamlConfig } from '@graphql-mesh/types'
-import { DefaultLogger } from '@graphql-mesh/utils'
+//import { DefaultLogger } from '@graphql-mesh/utils'
 import { load } from 'js-yaml'
 import { readFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 import { Logger } from '../utils/logger'
 
-const logger = new DefaultLogger()
+//const logger = new DefaultLogger()
 
 // Load the config.yaml file
 export const getConfig = (): YamlConfig.Config => {
 	Logger.debug('CONFIG', 'getConfig', 'Loading config file')
 	let config: YamlConfig.Config
-
+	let sourcesConfig: YamlConfig.Config
+	const configFile = process.env['ConfigFile'] || '../../config/config.yaml'
 	try {
-		const configPath = resolve('./config.yaml')
+		const configPath = resolve(configFile)
 		config = load(readFileSync(configPath, { encoding: 'utf-8' }))
 	} catch (e) {
-		Logger.error('CONFIG', 'getConfig', "Failed loading config ", e)
-		throw new Error('FAiles laoding config file ./config.yaml', e)
+		Logger.error('CONFIG', 'getConfig', "Failed loading config " + configFile, e)
+		throw new Error('FAiles laoding config file ' + configFile, e)
 	}
 
 	if (!config) {
-		Logger.error('CONFIG', 'getConfig', "No config loaded")
-		throw new Error('No configuration')
+		Logger.error('CONFIG', 'getConfig', "No config loaded from " + configFile)
+		throw new Error('No configuration loaded from ' + configFile)
 	}
-	if (config.sources) {
-		Logger.info('CONFIG', 'getConfig', 'Config file loaded successfully with ' + config.sources.length + " sources")
+
+	// if sources config is on a separate file define by SourcesConfigFile env var
+	const sourcesConfigFile = process.env['SourcesConfigFile']
+	if (sourcesConfigFile) {
+		const sourcesConfigFile = process.env['SourcesConfigFile'] || '../../config/sourcesConfig.yaml'
+		try {
+			const sourcesConfigPath = resolve(sourcesConfigFile)
+			sourcesConfig = load(readFileSync(sourcesConfigPath, { encoding: 'utf-8' }))
+		} catch (e) {
+			Logger.error('CONFIG', 'getConfig', "Failed loading sources Config " + sourcesConfigFile, e)
+			throw new Error('FAiles laoding sources config file' + sourcesConfigFile, e)
+		}
+		if (sourcesConfig.sources) {
+			Logger.info('CONFIG', 'getConfig', 'sources Config file loaded successfully with ' + sourcesConfig.sources.length + " sources")
+		} else {
+			Logger.error('CONFIG', 'getConfig', 'sources Config file loaded successfully but without sources')
+			throw new Error('FAiles laoding sources config file no sources found' + sourcesConfigFile)
+		}
+		config['sources'] = sourcesConfig.sources
 	} else {
-		Logger.warn('CONFIG', 'getConfig', 'Config file loaded successfully but without sources')
+		if (!config.sources) {
+			Logger.error('CONFIG', 'getConfig', "No source defined in configuration file" + configFile)
+			throw new Error('No source defioned in configuration file ' + configFile)
+		}
 	}
 	return config
 }
