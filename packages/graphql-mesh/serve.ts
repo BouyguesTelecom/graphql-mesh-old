906958fa1fa48571e0822cc6fc3e87e0853451db
Thierry DEGREMONT <thdegrem@bouyguestelecom.fr>
2024-08-02 16:25:32 +0200

    add dynamic import to load mesh on MESH_ARTIFACTS_DIRNAME env var

--- packages/graphql-mesh/serve.ts
+++ packages/graphql-mesh/serve.ts
@@ -1,12 +1,14 @@
 import { createServer } from 'node:http'
-import { createBuiltMeshHTTPHandler } from './var/.mesh'
+//import { createBuiltMeshHTTPHandler } from './var/.mesh'
 import { getConfig } from './utils/parseYamlConfig'
 import { Logger } from './utils/logger'
-const config = getConfig()
-
-const PORT = config.serve?.port ?? 4000
-const HOSTNAME = config.serve?.hostname ?? 'http://0.0.0.0'
-
-const server = createServer(createBuiltMeshHTTPHandler())
-Logger.info('STARTUP','main',`🚀 Server ready at ${HOSTNAME}:${PORT}/graphql`)
-server.listen(PORT)
+const mesh = process.env['MESH_ARTIFACTS_DIRNAME'] || ".mesh"
+import(mesh)
+	.then(obj => {
+		const config = getConfig()
+		const PORT = config.serve?.port ?? 4000
+		const HOSTNAME = config.serve?.hostname ?? 'http://0.0.0.0'
+		const server = createServer(obj.createBuiltMeshHTTPHandler())
+		Logger.info('STARTUP', 'main', `🚀 Server ready at ${HOSTNAME}:${PORT}/graphql`)
+		server.listen(PORT)
+	})
